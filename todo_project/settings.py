c93434c8294e5d57c6502c0853aed7110e64db26
Nourin <nourinkt0098@gmail.com>
2023-12-15 17:44:10 +0530

    update requirements.txt

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!vt70y#d&izfv3m8%b13x9h$*l9f^_5%hb#nq33zut^tj=$%4o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
